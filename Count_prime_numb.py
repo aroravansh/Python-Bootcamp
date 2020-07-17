df03a0b69d2c1aae3c85c8b44a042934913c5e2d
Vansh <44532230+aroravansh@users.noreply.github.com>
2020-07-17 20:48:19 +0530

    Add files via upload
    A code to count all the prime numbers till the number entered

---
+++ Count_prime_numb.py
@@ -0,0 +1,18 @@
+def count_primes(num):
+    primes = [2]
+    x = 3
+    if num < 2:
+        return 0
+    while x <= num:
+        for y in primes:  # use the primes list!
+            if x%y == 0:
+                x += 2
+                break
+        else:
+            primes.append(x)
+            x += 2
+    print(primes)
+    return len(primes)
+x = input('Enter a number till which you want to calc prime numb ')
+c = count_primes(int(x))
+print(f'There are {c} prime numbers')
